7b66ecfa5be24e2a71830179efb8bd6f52eea167
Fahim Ahmed Emon <ahmed.fahim37@gmail.com>
2024-03-16 10:42:33 +0600

    device model updated

--- device/models.py
+++ device/models.py
@@ -5,10 +5,8 @@ from account.models import Company
 class Device(models.Model):
     company = models.ForeignKey(Company, on_delete=models.CASCADE)
     device_type = models.CharField(max_length=255)
-    serial_number = models.CharField(max_length=255, unique=True)
     condition_on_checkout = models.TextField(blank=True)
+    checked_out = models.BooleanField(default=False)
 
     def __str__(self):
-        return (
-            f"{self.company.company_name} - {self.device_type} ({self.serial_number})"
-        )
+        return f"{self.company.company_name} - {self.device_type}"
